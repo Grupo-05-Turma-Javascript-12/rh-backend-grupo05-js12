83260eb990d6642b70d5a42a59985c19fbc233c4
João Henrique <jhenrique.caval@hotmail.com>
2026-01-16 15:54:46 -0300

    fix: some imports fix

--- src/funcionario/funcionario.module.ts
+++ src/funcionario/funcionario.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FuncionarioController } from './controllers/funcionario.controller';
 import { Funcionario } from './entities/funcionario.entity';
 import { FuncionarioService } from './services/funcionario.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Funcionario])],
-  controllers: [],
+  controllers: [FuncionarioController],
   providers: [FuncionarioService],
   exports: [],
 })
