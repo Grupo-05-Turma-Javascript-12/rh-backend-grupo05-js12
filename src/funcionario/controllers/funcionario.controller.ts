e870abdfbff61f12c348cdd7a8a03cb0f15a499b
João Henrique <jhenrique.caval@hotmail.com>
2026-01-16 16:03:12 -0300

    fix: findByStatus solved

--- src/funcionario/controllers/funcionario.controller.ts
+++ src/funcionario/controllers/funcionario.controller.ts
@@ -1,49 +1,61 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
-import { FuncionarioService } from "../services/funcionario.service";
-import { Funcionario } from "../entities/funcionario.entity";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Funcionario } from '../entities/funcionario.entity';
+import { FuncionarioService } from '../services/funcionario.service';
 
 @Controller('/funcionarios')
 export class FuncionarioController {
-    constructor(private readonly funcionarioService: FuncionarioService) {}
-    @Get()
-    @HttpCode(HttpStatus.OK)
-    findAll(): Promise<Funcionario[]> {
-        return this.funcionarioService.findAll()
-    }
+  constructor(private readonly funcionarioService: FuncionarioService) {}
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Funcionario[]> {
+    return this.funcionarioService.findAll();
+  }
 
-    @Get('/:id')
-    @HttpCode(HttpStatus.OK)
-    findById(@Param('id', ParseIntPipe) id: number): Promise<Funcionario | null> {
-        return this.funcionarioService.findById(id)
-    }
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Funcionario | null> {
+    return this.funcionarioService.findById(id);
+  }
 
-    @Get('/setor/:setor')
-    @HttpCode(HttpStatus.OK)
-    findBySetor(@Param('setor') setor: string): Promise<Funcionario[]> {
-        return this.funcionarioService.findBySetor(setor)
-    }
+  @Get('/setor/:setor')
+  @HttpCode(HttpStatus.OK)
+  findBySetor(@Param('setor') setor: string): Promise<Funcionario[]> {
+    return this.funcionarioService.findBySetor(setor);
+  }
 
-    @Get('/status/:status')
-    @HttpCode(HttpStatus.OK)
-    findByStatus(@Param('status') status: boolean): Promise<Funcionario[]> {
-        return this.funcionarioService.findByStatus(status)
-    }
+  @Get('/status/:status')
+  @HttpCode(HttpStatus.OK)
+  findByStatus(@Param('status') status: string): Promise<Funcionario[]> {
+    const ativo = status === 'true';
+    return this.funcionarioService.findByStatus(ativo);
+  }
 
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    create(@Body() funcionario: Funcionario): Promise<Funcionario> {
-        return this.funcionarioService.create(funcionario)
-    }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() funcionario: Funcionario): Promise<Funcionario> {
+    return this.funcionarioService.create(funcionario);
+  }
 
-    @Put()
-    @HttpCode(HttpStatus.OK)
-    update(@Body() funcionario: Funcionario): Promise<Funcionario> {
-        return this.funcionarioService.update(funcionario)
-    }
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() funcionario: Funcionario): Promise<Funcionario> {
+    return this.funcionarioService.update(funcionario);
+  }
 
-    @Delete('/:id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return this.funcionarioService.delete(id)
-    }
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.funcionarioService.delete(id);
+  }
 }
