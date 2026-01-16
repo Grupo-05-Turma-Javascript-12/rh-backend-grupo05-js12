72d70fd6dc6a700db3ee661f0fa4da2ab19f22f3
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-01-16 11:43:55 -0300

    feat: funcionario service updated

---
+++ src/funcionario/controllers/funcionario.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Delete, Get, HttpCode, HttpStatus, Post, Put } from "@nestjs/common";
+import { FuncionarioService } from "../services/funcionario.service";
+
+@Controller('/funcionarios')
+export class Funcionario {
+    constructor(private readonly funcionarioService: FuncionarioService) {}
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Funcionario[]> {}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findById(): Promise<Funcionario> {}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findBySetor(): Promise<Funcionario> {}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findByStatus(): Promise<Funcionario> {}
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(): Promise<Funcionario> {}
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(): Promise<Funcionario> {}
+
+    @Delete()
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(): void {}
+}
